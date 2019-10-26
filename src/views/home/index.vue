fd305377879221231bc46616ca725b064e24e583
alonesu <1225461992@qq.com>
2019-10-26 19:12:08 +0800

    首页完成。侧边栏导航

---
+++ src/views/home/index.vue
@@ -0,0 +1,126 @@
+<template>
+  <el-container class="container-home">
+    <el-aside :width="isOpen ? '64px' : '200px' ">
+      <div class="logo" :class="{logoSmall : isOpen}"></div>
+      <el-menu
+        default-active="/"
+        class="el-menu-vertical-demo"
+        background-color="#002033"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+        :collapse="isOpen"
+        :collapse-transition="false"
+        router
+      >
+        <el-menu-item index="/">
+          <i class="el-icon-s-home"></i>
+          <span slot="title">首页</span>
+        </el-menu-item>
+        <el-menu-item index="/article">
+          <i class="el-icon-document"></i>
+          <span slot="title">内容管理</span>
+        </el-menu-item>
+        <el-menu-item index="/image">
+          <i class="el-icon-picture"></i>
+          <span slot="title">素材管理</span>
+        </el-menu-item>
+        <el-menu-item index="/publish">
+          <i class="el-icon-s-promotion"></i>
+          <span slot="title">发布文章</span>
+        </el-menu-item>
+        <el-menu-item index="/comment">
+          <i class="el-icon-chat-dot-round"></i>
+          <span slot="title">评论管理</span>
+        </el-menu-item>
+        <el-menu-item index="/fans">
+          <i class="el-icon-present"></i>
+          <span slot="title">粉丝管理</span>
+        </el-menu-item>
+        <el-menu-item index="/setting">
+          <i class="el-icon-setting"></i>
+          <span slot="title">个人设置</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
+    <el-container>
+      <el-header>
+        <span @click="open" class="el-icon-s-fold icon"></span>
+        <span class="text">江苏传智播客科技教育有限公司</span>
+        <el-dropdown class="dropdown">
+          <span class="el-dropdown-link">
+            <img src="../../assets/images/avatar.jpg" alt />
+            <span>unsername</span>
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item icon="el-icon-setting">个人设置</el-dropdown-item>
+            <el-dropdown-item icon="el-icon-unlock">退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      isOpen: false
+    }
+  },
+  methods: {
+    open () {
+      this.isOpen = !this.isOpen
+    }
+  }
+}
+</script>
+
+<style scoped lang='less'>
+.container-home {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  .el-aside {
+    background: #002033;
+    .logo {
+      width: 100%;
+      height: 60px;
+      background: #002344 url(../../assets/images/logo_admin.png) no-repeat
+        center / 140px auto;
+    }
+    .logoSmall {
+      background: url(../../assets/images/logo_admin_01.png) no-repeat center / 36px auto;
+    }
+    .el-menu {
+      border-right: none;
+    }
+  }
+  .el-header {
+    border-bottom: 1px solid #ddd;
+    line-height: 60px;
+    .icon {
+      font-size: 30px;
+      vertical-align: middle;
+    }
+    .text {
+      vertical-align: middle;
+    }
+    .dropdown {
+      float: right;
+      img {
+        width: 36px;
+        display: inline-block;
+        vertical-align: middle;
+        margin: 0 5px 0 0;
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/welcome/index.vue
@@ -0,0 +1,16 @@
+<template>
+
+    <img src="../../assets/images/welcome.jpg" alt="">
+
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+img {
+    display: block;
+    margin: 0 auto;
+}
+</style>
