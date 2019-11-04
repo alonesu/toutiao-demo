bb7c718e25740933a439cc3dd3d3c81438242ac3
alonesu <1225461992@qq.com>
2019-11-04 18:37:29 +0800

    发布文章完成

--- src/components/my-image.vue
+++ src/components/my-image.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="my-image">
     <div class="btn_box" @click="open">
-      <img src="../assets/images/default.png" alt />
+      <img :src="value || btnImage" alt />
     </div>
     <!-- 对话框 -->
     <el-dialog :visible.sync="dialogVisible" width="750px">
       <el-tabs v-model="activeName" type="card">
-        <el-tab-pane label="素材库" name="true">
+        <el-tab-pane label="素材库" name="image">
           <div>
             <el-radio-group @change="toggleList" v-model="images.collect" size="small">
               <el-radio-button :label="false">全部</el-radio-button>
@@ -15,7 +15,13 @@
           </div>
           <!-- 图片列表 -->
           <div class="img_list">
-            <div class="item" :class="{selected: selectedImageUrl === item.url}" v-for="item in imagesList" :key="item.id" @click="selected(item.url)">
+            <div
+              class="item"
+              :class="{selected: selectedImageUrl === item.url}"
+              v-for="item in imagesList"
+              :key="item.id"
+              @click="selected(item.url)"
+            >
               <img :src="item.url" />
             </div>
           </div>
@@ -28,31 +34,33 @@
             @current-change="pager"
           ></el-pagination>
         </el-tab-pane>
-        <el-tab-pane label="上传图片" name="false">
-            <el-upload
-              class="avatar-uploader"
-              action="http://ttapi.research.itcast.cn/mp/v1_0/user/images"
-              :show-file-list="false"
-              :on-success="addSuccess"
-              :headers="headers"
-              name="image"
-            >
-              <img v-if="uploadImageUrl" :src="uploadImageUrl" class="avatar" />
-              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-            </el-upload>
+        <el-tab-pane label="上传图片" name="upload">
+          <el-upload
+            class="avatar-uploader"
+            action="http://ttapi.research.itcast.cn/mp/v1_0/user/images"
+            :show-file-list="false"
+            :on-success="addSuccess"
+            :headers="headers"
+            name="image"
+          >
+            <img v-if="uploadImageUrl" :src="uploadImageUrl" class="avatar" />
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
         </el-tab-pane>
       </el-tabs>
 
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+        <el-button type="primary" @click="confirmImage">确 定</el-button>
       </span>
     </el-dialog>
   </div>
 </template>
 
 <script>
+import defaultImage from '../assets/images/default.png'
 export default {
+  props: ['value'],
   data () {
     return {
       dialogVisible: false,
@@ -65,18 +73,25 @@ export default {
       imagesList: [],
       //   总条数
       total: 0,
-      activeName: 'true',
+      // 确认是素材库还是上传图片
+      activeName: 'image',
       //  请求头
       headers: {
-        Authorization: `Bearer ${JSON.parse(window.sessionStorage.getItem('hm-toutiao-pc-user-key')).token}`
+        Authorization: `Bearer ${
+          JSON.parse(window.sessionStorage.getItem('hm-toutiao-pc-user-key'))
+            .token
+        }`
       },
       // 上传的图片地址
       uploadImageUrl: '',
       // 选中图片地址
-      selectedImageUrl: ''
+      selectedImageUrl: '',
+      // 按钮图片
+      btnImage: defaultImage
     }
   },
   methods: {
+    // 打开对话框
     open () {
       this.dialogVisible = true
       this.getImages()
@@ -102,45 +117,71 @@ export default {
     // 上传成功后执行的函数
     addSuccess (res) {
       this.uploadImageUrl = res.data.url
+      // 提示
       this.$message.success('上传成功')
     },
     // 选中图片
     selected (url) {
       this.selectedImageUrl = url
+    },
+    // 确认图片
+    confirmImage () {
+      if (this.activeName === 'image') {
+        if (!this.selectedImageUrl) {
+          return this.$message.warning('请选择一张图片')
+        }
+        // this.btnImage = this.selectedImageUrl
+        // 提交给父组件
+        this.$emit('input', this.selectedImageUrl)
+        this.dialogVisible = false
+      } else {
+        if (!this.uploadImageUrl) {
+          return this.$message.warning('请上传一张图片')
+        }
+        // this.btnImage = this.uploadImageUrl
+        // 提交给父组件
+        this.$emit('input', this.uploadImageUrl)
+        this.dialogVisible = false
+      }
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-.btn_box {
-  width: 150px;
-  height: 150px;
-  border: 1px dashed #ddd;
-  img {
-    width: 100%;
-    height: 100%;
-    display: block;
-  }
-}
-.item {
-  width: 150px;
-  height: 120px;
-  position: relative;
+.my-image {
   display: inline-block;
-  margin: 0 20px 10px  0 ;
-  img {
-    width: 100%;
-    height: 100%;
+  margin-right: 20px;
+  .btn_box {
+    width: 150px;
+    height: 150px;
+    border: 1px dashed #ddd;
+    img {
+      width: 100%;
+      height: 100%;
+      display: block;
+    }
   }
-  &.selected::after {
-      content: '';
+  .item {
+    width: 150px;
+    height: 120px;
+    position: relative;
+    display: inline-block;
+    margin: 0 20px 10px 0;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+    &.selected::after {
+      content: "";
       position: absolute;
       top: 0;
       left: 0;
       width: 100%;
       height: 100%;
-      background: rgba(0,0,0,.3) url(../assets/images/selected.png) no-repeat center / 50px;
+      background: rgba(0, 0, 0, 0.3) url(../assets/images/selected.png)
+        no-repeat center / 50px;
+    }
   }
 }
 </style>
--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -2,32 +2,39 @@
   <div class="publish">
     <el-card>
       <div slot="header">
-        <my-bread>发布文章</my-bread>
+        <my-bread>{{$route.query.id ? '修改' : '发布'}}文章</my-bread>
       </div>
       <el-form label-width="80px">
-        <el-form-item label="标题:" v-model="articleForm.title">
-          <el-input style="width:300px"></el-input>
+        <el-form-item label="标题:">
+          <el-input style="width:300px" v-model="articleForm.title"></el-input>
         </el-form-item>
         <el-form-item label="内容:">
           <quill-editor v-model="articleForm.content" :options="editorOption"></quill-editor>
         </el-form-item>
         <el-form-item label="封面:">
-          <el-radio-group v-model="articleForm.cover.type">
+          <el-radio-group v-model="articleForm.cover.type" @change="articleForm.cover.images=[]">
             <el-radio :label="1">单图</el-radio>
             <el-radio :label="3">三图</el-radio>
             <el-radio :label="0">无图</el-radio>
             <el-radio :label="-1">自动</el-radio>
           </el-radio-group>
-            <div>
-              <my-image></my-image>
-            </div>
+          <div v-if="articleForm.cover.type === 1">
+            <my-image v-model="articleForm.cover.images[0]"></my-image>
+          </div>
+          <div v-if="articleForm.cover.type === 3">
+            <my-image v-for="i in 3" :key="i" v-model="articleForm.cover.images[i-1]"></my-image>
+          </div>
         </el-form-item>
         <el-form-item label="频道:">
           <my-channel v-model="articleForm.channel_id"></my-channel>
         </el-form-item>
-        <el-form-item size="small" style="float:right">
-          <el-button type="primary">发表</el-button>
-          <el-button>存入草稿</el-button>
+        <el-form-item size="small" style="float:right" v-if="$route.query.id">
+          <el-button type="primary" @click="update(false)">修改</el-button>
+          <el-button @click="update(true)">存入草稿</el-button>
+        </el-form-item>
+        <el-form-item size="small" style="float:right" v-else>
+          <el-button type="primary" @click="create(false)">发表</el-button>
+          <el-button @click="create(true)">存入草稿</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -52,8 +59,8 @@ export default {
         content: '',
         channel_id: null,
         cover: {
-          type: null,
-          images: null
+          type: 1,
+          images: []
         }
       },
       //   富文本配置对象
@@ -70,6 +77,61 @@ export default {
           ]
         }
       }
+    }
+  },
+  methods: {
+    // 发布文章
+    async create (draft) {
+      await this.$http.post(`articles?draft=${draft}`, this.articleForm)
+      this.$message.success(draft ? '存入草稿成功' : '发表成功')
+      this.$router.push('/article')
+    },
+    // 获取文章
+    async getArticle (id) {
+      const {
+        data: { data }
+      } = await this.$http.get('articles/' + id)
+      // 表单填充 面包屑文字 按钮文字
+      this.articleForm = data
+    },
+    // 修改文章
+    async update (draft) {
+      await this.$http.put(
+        `articles/${this.articleForm.id}?draft=${draft}`,
+        this.articleForm
+      )
+      // 提示
+      this.$message.success('修改成功')
+      // 去内容管理
+      this.$router.push('/article')
+    },
+    // 切换文章状态 发布。编辑
+    toggleArticleStatus () {
+      // 判断当前是否是编辑
+      const articleId = this.$route.query.id
+      if (articleId) {
+        // 获取当前文章信息
+        this.getArticle(articleId)
+      } else {
+        // 重置数据不能为空对象 模块中articleForm.cover.images
+        this.articleForm = {
+          title: '',
+          content: '',
+          channel_id: null,
+          cover: {
+            type: 1,
+            images: []
+          }
+        }
+      }
+    }
+  },
+  created () {
+    this.toggleArticleStatus()
+  },
+  watch: {
+    '$route.query.id': function () {
+      this.toggleArticleStatus()
     }
   }
 }
